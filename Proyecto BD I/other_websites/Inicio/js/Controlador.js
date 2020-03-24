e4d08ceafad8bf28b91a3eced57290d4bb04ef88
LPSoldier19 <rafael.bautista1@hotmail.es>
2020-03-24 17:32:13 -0600

    2

--- Proyecto BD I/other_websites/Inicio/js/Controlador.js
+++ Proyecto BD I/other_websites/Inicio/js/Controlador.js
@@ -16,36 +16,46 @@ function FacturasPorPagar(){
         <input type="button" value="Facturas Por Pagar" onclick="FacturasPorPagar()" class="File_accion">
     `));
     $('#Contenido').html($(`
-        <div class="col-12 row" id="color-tittle-list-bills-pay">
-            <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><h2 class="sizeLetters">Nombre Proveedor</h2></div>
+        <div class="col-12 row text-center" id="color-tittle-list-bills-pay">
+            <div class="col-sm-6 col-md-6 col-lg-6 col-xl-2 col-6 "><h2 class="sizeLetters">Nombre Proveedor</h2></div>
             <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">Facturas</h2></div>
             <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">fecha emitida</h2></div>
             <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">Nombre Limited</h2></div>
-            <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Estado</h2></div>
             <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Monto</h2></div>
             <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">ISV</h2></div>
+            <div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 col-4 "><h2 class="sizeLetters">Estado</h2></div>
         </div>
         <div class="line-contenido"></div>
     `))
     $('#Contenido').append($(`
         <div class="col-12" id="List-style-bills-pay" >
-            <div class="col-12 row" id="color-descriptionA-list-bills-pay">
-                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="Truper"></div>
+            <div class="col-12 row text-center" id="color-descriptionA-list-bills-pay">
+                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-2 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="Truper"></div>
                 <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="00000102001"></div>
                 <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">11/01/2020</h2></div>
                 <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">26/01/2020</h2></div>
-                <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Pagado</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">10,582.50</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">1,587.37</h2></div>
+                <div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 col-4 ">
+                <select id="slc-estado" class="form-control pl-0 pr-0 ml-0 mr-0 mt-1 mb-2" style="font-size:15px;">
+                <option>Pagado</option>
+                <option>Pendiente</option>
+                </select>
+                </div>
             </div>
-            <div class="col-12 row" id="color-descriptionB-list-bills-pay">
-                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="CementosX"></div>
+            <div class="col-12 row text-center" id="color-descriptionB-list-bills-pay">
+                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-2 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="CementosX"></div>
                 <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="00000102002"></div>
                 <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">12/03/2020</h2></div>
                 <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">12/04/2020</h2></div>
-                <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Pendiente</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">25,785.30</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">3,867.79</h2></div>
+                <div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 col-4 ">
+                <select id="slc-estado" class="form-control pl-0 pr-0 ml-0 mr-0 mt-1 mb-2" style="font-size:15px;">
+                <option>Pagado</option>
+                <option>Pendiente</option>
+                </select>
+                </div>
             </div>
         </div>
     `))
