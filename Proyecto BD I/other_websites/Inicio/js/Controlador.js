0451d27f4ed6f039728ac112f043ba1a058df843
Edwin Quintero <EdwinQuintero01246>
2020-03-24 16:16:03 -0600

    commits

--- Proyecto BD I/other_websites/Inicio/js/Controlador.js
+++ Proyecto BD I/other_websites/Inicio/js/Controlador.js
@@ -18,9 +18,9 @@ function FacturasPorPagar(){
     $('#Contenido').html($(`
         <div class="col-12 row" id="color-tittle-list-bills-pay">
             <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><h2 class="sizeLetters">Nombre Proveedor</h2></div>
-            <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">Facturas</h2></div>
-            <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">fecha emitida</h2></div>
-            <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">Nombre Limited</h2></div>
+            <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">Facturas</h2></div>
+            <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">fecha emitida</h2></div>
+            <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">Nombre Limited</h2></div>
             <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Estado</h2></div>
             <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Monto</h2></div>
             <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">ISV</h2></div>
@@ -30,19 +30,19 @@ function FacturasPorPagar(){
     $('#Contenido').append($(`
         <div class="col-12" id="List-style-bills-pay" >
             <div class="col-12 row" id="color-descriptionA-list-bills-pay">
-                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><h2 class="sizeLetters">Truper</h2></div>
-                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">00000102001</h2></div>
-                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">11/01/2020</h2></div>
-                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">26/01/2020</h2></div>
+                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="Truper"></div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="00000102001"></div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">11/01/2020</h2></div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">26/01/2020</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Pagado</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">10,582.50</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">1,587.37</h2></div>
             </div>
             <div class="col-12 row" id="color-descriptionB-list-bills-pay">
-                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><h2 class="sizeLetters">CementosX</h2></div>
-                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">00000102002</h2></div>
-                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">12/03/2020</h2></div>
-                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-1 col-6 "><h2 class="sizeLetters">12/04/2020</h2></div>
+                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="CementosX"></div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><input type="button" onclick="infoBillsPay()" class="sizeLetters Button-bills-provider" Value="00000102002"></div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">12/03/2020</h2></div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-6 "><h2 class="sizeLetters">12/04/2020</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">Pendiente</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">25,785.30</h2></div>
                 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-1 col-4 "><h2 class="sizeLetters">3,867.79</h2></div>
@@ -74,6 +74,15 @@ function Clientes(){
 function Productos(){
     $('#File_position').html($(`
         <input type="button" value="Productos" class="File_accion">/
+    `));
+    $('#Contenido').html($(`
+    `))
+}
+function infoBillsPay(){
+    FacturasPorPagar();
+    $('#File_position').append($(`
+        <span>/</span>
+        <input type="button" value="información Factura" onclick="infoBillsPay()" class="File_accion">
     `));
     $('#Contenido').html($(`
     `))
