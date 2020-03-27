4f6890b088c6c25d05863134b3b4fdce39633e20
JavierAmaya <photojavhn@gmail.com>
2020-03-26 20:59:13 -0600

    cambio de flujo

--- Proyecto BD I/js/controlador.js
+++ Proyecto BD I/js/controlador.js
@@ -1,3 +1,27 @@
-$('#myCarousel').on('slide.bs.carousel', function () {
-    // do something…
-})
+$("#enviar-compra").click(function(){
+    console.log("entro");
+
+
+    $.ajax({
+        url:"../ajax/usuario.php?accion=almacenarCompraUsuario",
+        dataType: "json",
+        method:"POST",
+        
+        
+        success: function(respuesta){
+            
+            console.log(respuesta);
+            
+            
+            
+        },
+        error:function(error){
+            console.log("valio");
+        }
+    })
+});
+
+$( "#btn-inicio-sesion" ).click(function() {
+    //alert( "Handler for .click() called." );
+    window.location.href = "index.html";
+  });
